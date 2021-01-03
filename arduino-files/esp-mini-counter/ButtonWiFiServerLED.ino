0a85360eee2638183f4132b7ed44bfe7aa22816d
kornh <kornelius@hellwich.me>
2021-01-03 20:00:15 +0100

    Change: Added HTTP Client to Arduino Code

--- arduino-files/esp-mini-counter/ButtonWiFiServerLED.ino
+++ arduino-files/esp-mini-counter/ButtonWiFiServerLED.ino
@@ -1,7 +1,7 @@
 #ifdef _BWSL
 
 // Secret data in dedicated file to hide from git
-#include "credentials.h" 
+#include "credentials.h"
 // Must contain "ssid" and "password" variables with type "char*"
 // On Error create file "credentials.h" with content:
 // const char* ssid     = "Name of SSID";
@@ -16,93 +16,124 @@ const int taster = D1;
 const int led_out_1 = D2;
 const int led_out_2 = D3;
 
+// HTTP Client objects
+HTTPClient http;
+String macAddress;
+
 int val = 0; // variable for reading the pin status
 
 void controller_setup()
 {
-    // Start Serial for Debugging
-    Serial.begin(115200);
+  // Start Serial for Debugging
+  Serial.begin(115200);
 }
 
 void output_init()
 {
-    pinMode(led_out_1, OUTPUT); // LED 1 PIN as Output
-    digitalWrite(led_out_1, LOW); // LED 1 Off
-    pinMode(led_out_2, OUTPUT); // LED 2 PIN as Output
-    digitalWrite(led_out_2, LOW); // LED 2 Off
+  pinMode(led_out_1, OUTPUT); // LED 1 PIN as Output
+  digitalWrite(led_out_1, LOW); // LED 1 Off
+  pinMode(led_out_2, OUTPUT); // LED 2 PIN as Output
+  digitalWrite(led_out_2, LOW); // LED 2 Off
 }
 
 void sensor_init()
 {
-    pinMode(taster, INPUT_PULLUP); // Sensor Input as PULLUP
-    digitalWrite(taster, HIGH); // Sensor on HIGH
+  pinMode(taster, INPUT_PULLUP); // Sensor Input as PULLUP
+  digitalWrite(taster, HIGH); // Sensor on HIGH
 }
 
 void status_init()
 {
-    digitalWrite(led_out_1, HIGH); // turn LED ON
-    digitalWrite(led_out_2, LOW); // turn LED ON
+  digitalWrite(led_out_1, HIGH); // turn LED ON
+  digitalWrite(led_out_2, LOW); // turn LED ON
 }
 
 void network_connect()
 {
-    // Connect to Wi-Fi network with SSID and password
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-    WiFi.begin(ssid, password);
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
 
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(500);
-        Serial.print(".");
-    }
-    // Print local IP address and start web server
-    Serial.println("");
-    Serial.println("WiFi connected.");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Print local MAC Address
+  macAddress = WiFi.macAddress();
+  Serial.println();
+  Serial.print("MAC: ");
+  Serial.println(macAddress);
 }
 
 void status_connected()
 {
-    digitalWrite(led_out_1, HIGH); // turn LED ON
-    digitalWrite(led_out_2, HIGH); // turn LED ON
+  digitalWrite(led_out_1, HIGH); // turn LED ON
+  digitalWrite(led_out_2, HIGH); // turn LED ON
 }
 
 void storage_init()
 {
+  http.begin("http://192.168.0.100:3000/api/register/" + macAddress);
+  int httpCode = http.GET();
+  if (httpCode > 0) { //Check the returning code
+
+    String payload = http.getString();   //Get the request response payload
+    Serial.println(payload);             //Print the response payload
+
+  }
+
+  http.end();   //Close connection
 }
 
 void status_ready()
 {
-    digitalWrite(led_out_1, LOW); // turn LED ON
-    digitalWrite(led_out_2, HIGH); // turn LED ON
+  digitalWrite(led_out_1, LOW); // turn LED ON
+  digitalWrite(led_out_2, HIGH); // turn LED ON
 }
 
 void sensor_read()
 {
-    val = digitalRead(taster); // read input value
-    // HIGH = no input
-    // LOW = user input
+  val = digitalRead(taster); // read input value
+  // HIGH = no input
+  // LOW = user input
 }
 
 int sensor_is_active()
 {
-    if(val != HIGH){ // HIGH = no input
-        return 1;
-    }
-    return 0;
+  if (val != HIGH) { // HIGH = no input
+    return 1;
+  }
+  return 0;
 }
 
 void sensor_on_active()
 {
-    digitalWrite(led_out_2, LOW); // turn LED ON
-    Serial.println(val);
+  digitalWrite(led_out_2, LOW); // turn LED ON
+  Serial.println(val);
+  http.begin("http://192.168.0.100:3000/api/count/" + macAddress);
+  int httpCode = http.GET();
+  if (httpCode > 0) { //Check the returning code
+
+    String payload = http.getString();   //Get the request response payload
+    Serial.println(payload);             //Print the response payload
+
+  }
+
+  http.end();   //Close connection
+
 }
 
 void sensor_on_inactive()
 {
-    digitalWrite(led_out_2, HIGH); // turn LED OFF
+  digitalWrite(led_out_2, HIGH); // turn LED OFF
 }
 
 #endif
