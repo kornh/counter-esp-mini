b050176d17eacbfb77a03298524285f7fd190bd5
kornh <kornelius@hellwich.me>
2021-02-22 11:54:09 +0100

    add Expose to dockerfile and data path for volumes

--- node-server/index.js
+++ node-server/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 
-const adapter = new FileSync('db.json')
+const adapter = new FileSync('data/db.json')
 const db = low(adapter)
 db.defaults({
         counterId: [],
